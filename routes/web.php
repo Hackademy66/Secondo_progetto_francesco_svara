915b365d0af124d84cc150d1cf0099a7f1e3e0f9
francescoSvara <francesco.svara@gmail.com>
2023-03-08 00:14:39 +0100

    modifica

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,12 @@ use App\Http\Controllers\VideogameController;
 |
 */
 
-
+// Route Public
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
 Route::get('/about', [PublicController::class, 'about'])->name('chi-siamo');
 Route::get('/contact', [PublicController::class, 'contact'])->name('contatti');
 Route::get('/services', [PublicController::class, 'services'])->name('servizi');
 
+// Route videogame
 Route::get('/videogames', [videogameController::class, 'videogame_index'])->name('videogames');
 Route::get('/videogames/dettaglio/{id}',  [videogameController::class, 'videogame_show'])->name('videogames-dettaglio');
